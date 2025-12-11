40f66f6d5e7693ef9d0526b28df8168de0ae4510
Hamed Khosravi <khosravi.hamed@okala.com>
2025-12-11 19:13:24 +0330

    first commit

--- melisa-ui/next.config.ts
+++ melisa-ui/next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   }
